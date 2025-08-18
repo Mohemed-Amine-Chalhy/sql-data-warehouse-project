060f3a1dc7672f82d813ef783631b9c7dd7b50e2
Mohamed Amine CHALHY <107750540+Mohemed-Amine-Chalhy@users.noreply.github.com>
2025-08-19 01:59:43 +0200

    Create procedure_load_bronze.sql

---
+++ scripts/bronze/procedure_load_bronze.sql
@@ -0,0 +1,105 @@
+/*
+-- ===================================================================================
+-- Stored Procedure: bronze.load_bronze
+-- ===================================================================================
+-- Purpose: This procedure performs a full reload of the bronze layer tables.
+--
+-- Key Actions:
+-- 1. Truncates existing data.
+-- 2. Bulk loads data from CSV files.
+-- 3. Handles errors and logs execution time.
+-- ===================================================================================
+*/
+
+
+ALTER   PROCEDURE [bronze].[load_bronze] AS 
+
+BEGIN 
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	SET @start_time = GETDATE();
+	BEGIN TRY
+		PRINT '======================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '======================================';
+
+		PRINT '--------------------------------------';
+		PRINT 'Loading CRM TABLES';
+		PRINT '--------------------------------------';
+		TRUNCATE TABLE  bronze.crm_cust_info;
+		BULK INSERT		bronze.crm_cust_info
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+
+
+		TRUNCATE TABLE  bronze.crm_prd_info;
+		BULK INSERT		bronze.crm_prd_info
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+
+		TRUNCATE TABLE  bronze.crm_sales_details;
+		BULK INSERT		bronze.crm_sales_details
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+
+		PRINT '--------------------------------------';
+		PRINT 'Loading ERP TABLES';
+		PRINT '--------------------------------------';
+
+		TRUNCATE TABLE  bronze.erp_cust_az12;
+		BULK INSERT		bronze.erp_cust_az12
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+
+
+		TRUNCATE TABLE  bronze.erp_loc_a101;
+		BULK INSERT		bronze.erp_loc_a101
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+
+		TRUNCATE TABLE  bronze.erp_px_cat_g1v2;
+		BULK INSERT		bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\CHALHY\Documents\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2 ,
+			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		);
+	SET @end_time = GETDATE();
+
+	END TRY
+	BEGIN CATCH
+	PRINT  '======================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+	PRINT 'ERROR MESSAGE ' + CAST(ERROR_MESSAGE() AS NVARCHAR);
+	PRINT 'ERROR NUMBER ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+	END CATCH
+
+	PRINT 'TIME TO LOAD	:' + CAST(DATEDIFF(second,  @start_time,	@end_time) AS NVARCHAR);
+END
+
